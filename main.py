d507c4cb42ef2c8677ba8abbd8c728f73d9591a0
mrs1669 <mrs1669.develop@gmail.com>
2021-04-13 06:23:47 +0900

    complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
-    #sympy.var('r')
-    #print(sympy.solve(DEu(r, 0.1), r))
-    print(DEu(-1.73, 0.1))
+    print('選択肢Bにスイッチした組番号 高い賞金が当たる確率p 閾値相対的リスク回避度')
+    for p in range(1, 10, 1):
+        print('            ', p+1, '            |        ', p/10, '        |         ', solve(p/10))
 
 def u(x: int, r: float):
     return (x**(1-r))/(1-r)
@@ -12,9 +12,20 @@ def Eu(x1: int, x2: int, r: float, p: float):
 def DEu(r: float, p: float):
     return Eu(200, 160, r, p)-Eu(385, 10, r, p)
 
-def solve(r: float):
+def solve(p: float):
     min: float = -10.0
     max: float = 10.0
-    return
+    count: int = 1000
+    small: float = min
+    large: float = max
+    temp: float = 0
+    while count > 0:
+        mid = (small+large)/2
+        if DEu(mid, p)>0:
+            large = mid
+        else:
+            small = mid
+        count-=1
+    return round(mid, 2)
 
 main()
